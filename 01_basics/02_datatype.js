75fd4bcebdbb46e7568ef003d8f25423aea16296
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-11 18:07:27 +0000

    datatype contaiiner done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code in newer version
+
+// alert(3 +3) we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+console.log("Monika")
+
+let name= "Monika"
+let age= 26
+let isLoggedIn= false
+let state;
+
+//number => 2 to power 53
+//bigint => but we cant use this
+//string => ""
+//null => standalone value(null is a type)
+//undefined
+//symbol =>unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
