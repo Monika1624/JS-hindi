79ed77b0b39647fe3f2c03b4ac7d92a101ce38a7
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-11 17:01:23 +0000

    Config container done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 90418
+let accountEmail= "monika@gmail.com"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+console.log(accountId)
+
+//accountId= 89789 not allowed
+
+accountEmail= "mannu@gmail.com"
+accountPassword= "897897"
+accountCity= "Jammu"
+console.table([accountId, accountEmail, accountPassword, accountCity])
