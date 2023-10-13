863cab35fc9bfd632fed1a8777ecb0e94cfc8f8b
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-13 15:21:06 +0000

    Datatype Container done

---
+++ 01_basics/03_Datatypeoperations.js
@@ -0,0 +1,11 @@
+let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+/*let score1= "33" => string
+console.log(typeof score1);
+console.log(typeof (score1));*/
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
