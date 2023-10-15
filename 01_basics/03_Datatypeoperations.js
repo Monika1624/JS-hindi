5dc03a5dbd4feaab3f69ce973f5d888c605c9034
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-15 06:25:14 +0000

    Conversion of datatype

--- 01_basics/03_Datatypeoperations.js
+++ 01_basics/03_Datatypeoperations.js
@@ -1,4 +1,4 @@
-let score = null
+let score = "Monika"
 // console.log(typeof score);
 // console.log(typeof (score));
 
@@ -7,5 +7,24 @@ console.log(typeof score1);
 console.log(typeof (score1));*/
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33"=> 33
+// "33abc"=> NaN, but type is number
+// null=> 0
+// undefined=> NaN
+// "Monika"=> NaN
+
+let ant= "Monika"
+
+let booleanAnt= Boolean(ant)
+// console.log(booleanAnt);
+
+// 1=> true, 0=> false
+// ""=> false, "Monika"=> true
+
+let someNumber= 16
+ let stringSomeNumber= String(someNumber)
+ console.log(stringSomeNumber);
+ console.log(typeof stringSomeNumber);
