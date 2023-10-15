85b594979f923b4445d23e88f03de13f1a23f6f4
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-15 08:16:20 +0000

    Conversion of datatype

--- 01_basics/03_Datatypeoperations.js
+++ 01_basics/03_Datatypeoperations.js
@@ -26,5 +26,31 @@ let booleanAnt= Boolean(ant)
 
 let someNumber= 16
  let stringSomeNumber= String(someNumber)
- console.log(stringSomeNumber);
- console.log(typeof stringSomeNumber);
+//  console.log(stringSomeNumber);
+//  console.log(typeof stringSomeNumber);
+
+/*****************************************/
+
+let value= 5
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+// console.log(2/2)
+
+let str1= "hello"
+let str2= "Monika"
+let str3= str1 + str2
+// console.log(str3);
+
+console.log(1+ "2");
+console.log("1" + 2);
+console.log(1+ 2+ "2");
+console.log(1+ "2"+ 2);
+console.log("1"+ 2+ 2);
+
+console.log((3+4) * 6 /9)
