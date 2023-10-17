94e87ff3f599bdf87361fffccc17878af70bb1ee
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-17 14:50:01 +0000

    String container done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name= "Monika"
+
+const age= 26
+/*
+  we can use modern type of syntax here, we used backtick and ${}=> STRING INTER-POLLUTION
+*/
+console.log(`My name is ${name} and my age is ${age}`);
