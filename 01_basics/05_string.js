aec8e27ab942d90abec9756aa08ebcdb8b90dc79
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-17 17:17:00 +0000

    string container done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,3 +5,5 @@ const age= 26
   we can use modern type of syntax here, we used backtick and ${}=> STRING INTER-POLLUTION
 */
 console.log(`My name is ${name} and my age is ${age}`);
+
+const gameName= new String('TempleRun') 
