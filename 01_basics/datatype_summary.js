f0f4c374ad68648367453e0de04c2c314a50d94b
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-17 14:16:29 +0000

    Datatype conatiner done

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,18 @@
+//primitive datatype
+
+// 7 => String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const Number= 100
+const anotherNumber= 99.9
+
+const isLoggedIn= false;
+
+const BigNumber= 345687973536687
+const Score= Symbol('123')
+const anotherScore= Symbol('123')
+console.log(Score==anotherScore)
+//Reference(Non-Primitive)
+
+// Array, String and Object
+
+//JavaScript is a dynamic type language because we dont declare the variable, it will decide to see the variable what type of variable is this during runtime.
