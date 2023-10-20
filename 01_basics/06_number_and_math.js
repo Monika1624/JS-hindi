cf329c08baa7be7d16652eec5bbb563b0723cc1b
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-20 15:34:09 +0000

    number and math done

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,9 @@
+const score= 100;
+console.log(score);
+/*
+ only difference is here
+ we cant take the new object and we can declare as number 
+*/
+
+const balance= new Number(100);
+console.log(balance);
