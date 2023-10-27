9d5b66982c60778a7145a7ae2afffdbdfea00e5b
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-27 16:32:52 +0000

    array done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,12 +1,12 @@
 //array
 
 const MyArr= [1,2,3,4,5,6]
-console.log(MyArr[3]);// using index
+//console.log(MyArr[3]);// using index
 
 const Hero= ["Tom", "Jerry", "Osworld"]
 
 const MyArr2= new Array(100, 99, 98, 97)
-console.log(MyArr2[1]);
+//console.log(MyArr2[1]);
 
 //Array Method
 
@@ -17,10 +17,10 @@ console.log(MyArr2[1]);
 //MyArr.unshift(100)
 //MyArr.shift()
 
-console.log(MyArr.includes(8));
+/*console.log(MyArr.includes(8));
 console.log(MyArr.indexOf(8));
 
 const newArr= MyArr.join()
 console.log(MyArr);
 console.log(newArr);
-console.log(typeof newArr);
+console.log(typeof newArr);*/
