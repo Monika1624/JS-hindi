9cd7994d4a7a2c7ab17d5c0536bc1332e73d266a
Monika1624 <96783274+Monika1624@users.noreply.github.com>
2023-10-27 13:13:27 +0000

    Array done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,9 @@
+//array
+
+const MyArr= [1,2,3,4,5,6]
+console.log(MyArr[3]);// using index
+
+const Hero= ["Tom", "Jerry", "Osworld"]
+
+const MyArr2= new Array(100, 99, 98, 97)
+console.log(MyArr2[1]);
