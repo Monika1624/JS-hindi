997837813a231981fee813482fb502fdcf2b7d15
Monika Thakur <monikathakur1624@gmail.com>
2023-10-28 15:57:14 +0530

    array done

--- 02_basics/03_array.js
+++ 02_basics/03_array.js
@@ -1,3 +1,33 @@
 const fruit = ["Apple", " Mango", "Orange"]
-fruit = ["Banana", "Mango","Orange" ]
-console.log(fruit);
+//fruit = ["Banana", "Mango","Orange" ] error
+
+fruit[0] = "Pear";
+//console.log(fruit);
+
+const vege =[]
+vege.push("onion")
+//console.log(vege);
+//fruit.pop()
+//console.log(fruit);
+
+fruit.splice(1, 1);
+//console.log(fruit);
+
+const myn= [1, 2, 3, 4, 5]
+myn.splice(2, 2);
+//console.log(myn);
+
+
+//Block scoped
+
+const movie = ["DDLJ", "KKKG", "KGF"]
+{
+    const movie2= movie.slice(0,3)
+    //const movie = ["Krish3", "KKKG"]
+    movie2.push("karanLovesMonika")
+    console.log(movie2);
+}
+//movie.push("karanLovesMonika")
+console.log(movie);
+
+
