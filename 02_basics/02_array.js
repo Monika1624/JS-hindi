566f80ca3e14724a04665adfac1f6a04e3d2449c
Monika Thakur <monikathakur1624@gmail.com>
2023-10-28 08:15:44 +0530

    array done

---
+++ 02_basics/02_array.js
@@ -0,0 +1,9 @@
+const marvel_heros= ["thor", "Ironman", "Spiderman"]
+const dc_heros= ["flash", "Shaktiman", "batman"]
+
+marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][0]);
+
+marvel_heros.concat(dc_heros)
+console.log(marvel_heros);
