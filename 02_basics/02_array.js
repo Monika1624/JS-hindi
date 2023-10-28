be3085feac7de1c02d929dc2e6cb165a3b0efe93
Monika Thakur <monikathakur1624@gmail.com>
2023-10-28 08:37:44 +0530

    array done

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -11,4 +11,14 @@ const allheros= marvel_heros.concat(dc_heros)
 
 //spread operator
 const all_new_heros= [...marvel_heros, ...dc_heros]
-console.log(all_new_heros);
+//console.log(all_new_heros);
+
+/*when u have array inside another array and u have to print in single output,
+then u have to use flat(depth)*/
+const another_array = [1, 2, 3, [4, 5, 6,] , 7, [6, 8, 9]]
+
+const new_another_array = another_array.flat(1);
+console.log(new_another_array);
+
+console.log(Array.isArray("Monika"));//ask question => isArray
+console.log(Array.from("Monika")); //change array into string =>from
