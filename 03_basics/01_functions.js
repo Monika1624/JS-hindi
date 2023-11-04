dd4072086ee33f67a0c4202198b0db38e2855395
Monika Thakur <monikathakur1624@gmail.com>
2023-11-04 16:27:52 +0530

    function done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function myName()
+{
+    console.log("M");
+    console.log("O");
+    console.log("N");
+    console.log("I");
+    console.log("K");
+    console.log("A");
+}
+myName
+
+function addTwoNumber(number1, number2)
+{
+    console.log(number1 + number2);
+}
+addTwoNumber(3, 5);
